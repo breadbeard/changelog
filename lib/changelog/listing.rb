07db72db5e1e4eac26e1feb6f7f62f97359948e3
Dwight van Tuyl <dvantuyl@gmail.com>
2012-01-21 22:17:05 -0800

    Listing outputs to string

--- lib/changelog/listing.rb
+++ lib/changelog/listing.rb
@@ -2,5 +2,24 @@
 module Changelog
 
   class Listing < Grit::Commit
+
+
+    def to_s
+      "* #{jirafy_message} #{githubify_id} (#{author.name})"
+    end
+
+    def jirafy_message
+      jira_base = "https://windermeresolutions.atlassian.net"
+      key_pattern = /([A-Z]+-\d+)/
+
+      message.gsub(key_pattern, '[\1](' + jira_base + '/browse/\1)')
+    end
+
+    def githubify_id
+      owner = "dvantuyl"
+      project = "changelog"
+
+      "[#{id[0,7]}](https://github.com/#{owner}/#{project}/#{id[0,7]})"
+    end
   end
 end
--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -10,14 +10,28 @@ module Changelog
     describe "#message" do
 
       it "returns the message of a commit" do
-        l.message.should == "ENG-5091: Test JIRA tag"
+        @listing.message.should == "ENG-5091: Test JIRA tag"
       end
     end
+
+    describe "#jirafy_message" do
+      it "adds jira links to commit message" do
+        @listing.jirafy_message.should == "[ENG-5091](https://windermeresolutions.atlassian.net/browse/ENG-5091): Test JIRA tag"
+      end
+    end
+
+    describe "#githubify_id" do
+      it "adds a github link to the commit id" do
+        @listing.githubify_id.should == "[3209c90](https://github.com/dvantuyl/changelog/3209c90)"
+      end
+    end
+
 
     describe "#to_s" do
 
       it "returns a pretty log listing of the commit" do
-
+        l = "* [ENG-5091](https://windermeresolutions.atlassian.net/browse/ENG-5091): Test JIRA tag [3209c90](https://github.com/dvantuyl/changelog/3209c90) (Dwight van Tuyl)"
+        @listing.to_s.should == l
       end
 
     end
